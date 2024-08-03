b47b4dcc02e9ee33b7a9275d78deeb1c055a91ea
NguyenHuuHung <hhnguyenit@gmail.com>
2024-08-03 12:23:57 +0700

    config file app js

--- app.js
+++ app.js
@@ -34,12 +34,10 @@ app.all("*", (req, res, next) => {
 });
 
 dotenv.config({ path: "./config.env" });
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
-
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
